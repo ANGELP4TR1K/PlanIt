31fd124be4bf62168a088e92517e3aa9d1ee586e
Gyalog Peti <gy.petitrefort@gmail.com>
2026-02-11 22:47:41 +0100

    Db végpontok, api (login,logout, register) végpontok

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 //!Module-ok importálása
 const express = require('express'); //?npm install express
 const session = require('express-session'); //?npm install express-session
+const MySQLStore = require('express-mysql-session')(session); //?npm install express-mysql-session
 const path = require('path');
 
 //!Beállítások
@@ -16,9 +17,20 @@ app.set('trust proxy', 1); //?Middleware Proxy
 //!Session beállítása:
 app.use(
     session({
+        key: 'planit_session',
         secret: 'titkos_kulcs', //?Ezt generálni kell a későbbiekben
+        store: new MySQLStore({
+            host: '127.0.0.1',
+            port: 3306,
+            user: 'root',
+            password: '',
+            database: 'planit'
+        }),
         resave: false,
-        saveUninitialized: true
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        }
     })
 );
 
