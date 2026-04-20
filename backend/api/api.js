3432f223caf233a93c8c89e93164ec58122d5548
Patrik <gallaipatrik@gmail.com>
2026-04-20 20:26:59 +0200

    hibajavitas

--- backend/api/api.js
+++ backend/api/api.js
@@ -106,7 +106,7 @@ router.post('/register', async (request, response) => {
 
         const usernameExists = await database.checkUsernameExists(username);
         if (usernameExists) return response.status(400).json({ message: 'A felhasználónév már foglalt.' });
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
         const result = await database.register(username, email, hashedPassword, full_name || null);
         const userId = result && result.insertId ? result.insertId : null;
         request.session.user = {
